aa40fde3b4b1affec7263ba71ef97a35f6db3dde
Ryan J <ryan.joseph@student.mec.cuny.edu>
2025-11-03 10:39:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,21 @@ st.set_page_config(
   page_icon= '👽',
   layout = 'wide'
 )
+
+# Custom CSS (optional - for styling)
+st.mardown('''        
+              <style>
+                  .main-haeder {front-size: 42px; font-weight: bold; text-align:center;}
+                  .sub-header {font_size: 24px; text-align:center; color: #666;}
+             </style>
+           ''', unsafe_allow_html = True)
+
+# Sidebar
+st. sidebar.title('📍 Navigation')
+page = st.sidebar.radio('Go to')
+                        ['🏠 Home', '😇About', '🧳 Projects', '🛠️ Skill', '📈b Resume',📩 Contact'])
+
+# Home Page
+if page =='🏠 Home':
+  st.mardown('<p class="main-header">Ryan Joseph</p>', unsafe_allow_html=True)
+  st.mardown('<p class="sub-header">Ryan Student | Medgar Evers College</p>', unsafe_allow_html=True)
